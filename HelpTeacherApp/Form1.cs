e255d4f8665262b3ed67ab79a57aa2b8ba6a34f8
Edgar Correa <coperedgar@gmail.com>
2024-04-04 18:47:53 -0600

    Cambio 2

--- HelpTeacherApp/Form1.cs
+++ HelpTeacherApp/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace HelpTeacherApp
 {
     public partial class Form1 : Form
     {
+        SqlConnection conexion = new SqlConnection(Properties.Settings.Default.HelpTeacherConnectionString);
         public Form1()
         {
             InitializeComponent();
